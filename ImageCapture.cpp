0cd306478becc2ce3f559f6cd921778c5fb99682
Andrew Ruf <andrewruf47@gmail.com>
2017-04-17 00:44:32 -0400

    Added Property Files

--- ImageCapture.cpp
+++ ImageCapture.cpp
@@ -1,5 +1,8 @@
 #include "ImageCapture.h"
+#include <Windows.h>
+#include <dshow.h>
 #include <mutex>
+#include <thread>
 
 class StereoCamera
 {
@@ -8,19 +11,45 @@ public:
 	StereoCamera();
 	~StereoCamera();
 
+	cv::Mat leftFrame;
+	cv::Mat leftFrameGray;
+	cv::Mat rightFrame;
+	cv::Mat rightFrameGray;
+
 private:
+	void updateFrames();
+
 	std::mutex imageLock;
+	std::thread cameraCaptureThread;
+	cv::VideoCapture leftCam;
+	cv::VideoCapture rightCam;
 };
 
 StereoCamera::StereoCamera()
 {
 }
 
+/**
+leftCamera - Left camera device number (As user must know what value this is)
+rightCamera - Right camera device number
+
+Initializes cameras and begins camera capture threads
+*/
 StereoCamera::StereoCamera(int leftCamera, int rightCamera)
 {
+	leftCam = cv::VideoCapture(leftCamera);
+	rightCam = cv::VideoCapture(rightCamera);
 }
 
 StereoCamera::~StereoCamera()
 {
+	leftCam.release();
+	rightCam.release();
+
+}
+
+void StereoCamera::updateFrames(){
+	imageLock.lock();
+	leftCam.s
 }
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 
-#include "ImageCapture.h"
+//#include "ImageCapture.h"
+#include <opencv2\opencv.hpp>
+#include <opencv2\gpu\gpu.hpp>
 #include <thread>
+#include <time.h>
 
 using namespace std;
 
@@ -14,15 +17,43 @@ int main(int argc, char * argv[]) {
 	thread speedLimitDetection;
 	thread lineDetection;
 	thread pedestrianDetection;
-
+	cv::gpu::CascadeClassifier_GPU HOG2;
 	cv::VideoCapture cam(0);
 	cv::VideoCapture cam2(1);
+	cv::CascadeClassifier HOG;
+	
+	//HOG.load("CarsOnly_5Stages_HOG.xml");
+	HOG2.load("haarcascade_eye.xml");
 
 	while (1) {
 		cam.read(leftFrame);
-		cam2.read(rightFrame);
+		cv::Mat gray;
+		cv::cvtColor(leftFrame,gray, cv::COLOR_BGR2GRAY);
+		//cam2.read(rightFrame);
+		std::vector<cv::Rect> pedsLeft;
+		cv::gpu::GpuMat pedsGpuLeft;
+		//std::vector<cv::Rect> pedsRight;
+		clock_t begin = clock();
+		cv::gpu::GpuMat gpuImg;
+		gpuImg.upload(gray);
+		int objNum = HOG2.detectMultiScale(gpuImg, pedsGpuLeft,2,4);
+		printf("Num Objs: %d\n", objNum);
+		HOG.detectMultiScale(leftFrame, pedsLeft);
+		//HOG.detectMultiScale(rightFrame, pedsRight);
+		cv::Mat obj_host;
+		pedsGpuLeft.colRange(0, objNum).download(obj_host);
+		cv::Rect* peds = obj_host.ptr<cv::Rect>();
+		for (int i = 0; i < objNum; i++) {
+			cv::rectangle(leftFrame,peds[i],cv::Scalar(255,0,0));
+		}
+		/*for (int i = 0; i < pedsRight.size(); i++) {
+			cv::rectangle(leftFrame, pedsRight[i], cv::Scalar(255, 0, 0));
+		}*/
 		cv::imshow("Camera Left", leftFrame);
-		cv::imshow("Camera Right", rightFrame);
+		clock_t after = clock();
+		printf("Elapsed Time: %f\n", (double)(after-begin)/CLOCKS_PER_SEC);
+
+		//cv::imshow("Camera Right", rightFrame);
 		cvWaitKey(1);
 	}
 
